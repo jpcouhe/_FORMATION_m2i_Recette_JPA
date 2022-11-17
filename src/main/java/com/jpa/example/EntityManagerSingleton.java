5ab57f52419911d216a1ed408d1af37c8b908b69
Jean-philippe COUHE <98466099+jpcouhe@users.noreply.github.com>
2022-11-17 14:05:42 +0100

    EntityManagerSingleton config

---
+++ src/main/java/com/jpa/example/EntityManagerSingleton.java
@@ -0,0 +1,18 @@
+package com.jpa.example;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public final class EntityManagerSingleton {
+
+    private static EntityManagerFactory ENTITY_INSTANCE;
+
+    private EntityManagerSingleton(){}
+
+    public static EntityManagerFactory getEntityInstance(){
+        if(ENTITY_INSTANCE == null){
+            ENTITY_INSTANCE = Persistence.createEntityManagerFactory("myPU");
+        }
+        return ENTITY_INSTANCE;
+    }
+}
